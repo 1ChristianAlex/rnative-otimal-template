d33329c0a54f7b70d74f8b56ed3a53916466d0ef
Christian Alexsander Santos <christianalexbh@hotmail.com>
2024-04-04 17:12:37 -0300

    ripple button

--- src/shared/components/ElevatedCard/ElevatedCard.tsx
+++ src/shared/components/ElevatedCard/ElevatedCard.tsx
@@ -1,16 +1,44 @@
-import React, { PropsWithChildren } from "react";
-import { View, ViewProps } from "react-native";
+import React, { PropsWithChildren, useState } from "react";
+import {
+	GestureResponderEvent,
+	TouchableNativeFeedback,
+	View,
+	ViewProps,
+} from "react-native";
+import colors from "tailwindcss/colors";
 
-const ElevatedCard: React.FC<PropsWithChildren<ViewProps>> = ({
+interface Props extends ViewProps {
+	onPress?: ((event: GestureResponderEvent) => void) | undefined;
+}
+
+const CardComponent: React.FC<PropsWithChildren<ViewProps>> = ({
 	children,
 	...props
 }) => (
 	<View
-		className="px-2 py-3 bg-white shadow-md shadow-black rounded-xl"
+		className="px-2 py-3 bg-white shadow-md shadow-black rounded-xl overflow-hidden"
 		{...props}
 	>
 		{children}
 	</View>
 );
+
+const ElevatedCard: React.FC<PropsWithChildren<Props>> = ({
+	onPress,
+	...props
+}) => {
+	if (!onPress) {
+		return React.createElement(CardComponent, props);
+	}
+
+	return (
+		<TouchableNativeFeedback
+			background={TouchableNativeFeedback.Ripple(colors.gray[300], false)}
+			onPress={onPress}
+		>
+			{React.createElement(CardComponent, props)}
+		</TouchableNativeFeedback>
+	);
+};
 
 export default ElevatedCard;
